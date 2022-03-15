2603a1c59627787d8a72d9cca9e93329b9e19fd7
= <maria.louisa.castro@gmail.com>
2022-03-15 23:58:28 +0000

    Add some psudo code for next time

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
 canvas.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.width);
+
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+// fillRect() params = (x start, y start, width, height)
+
+// add a score to the screen & highscore (local storage)
+
+const gameCountdown = () => {
+  //have a 3 2 1 count down with beeps!
+  //the call the function to start the game
+};
+
+//function for game play...
