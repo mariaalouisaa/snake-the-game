41d07c47b4517ce2f8f3f9177ca9bf475d0d67dd
= <maria.louisa.castro@gmail.com>
2022-03-17 16:42:47 +0000

    Add score text to canvas

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+// setting canvas background
 canvas.fillStyle = "black";
-
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 // fillRect() params = (x start, y start, width, height)
 
-// add a score to the screen & highscore (local storage)
+// displaying score on canvas
+ctx.font = "16px monospace";
+ctx.fillStyle = "white";
+ctx.textAlign = "center";
+ctx.fillText("score:", 370, 20);
+
+// want to add a score to the screen & highscore (local storage)
 
 const gameCountdown = () => {
   //have a 3 2 1 count down with beeps!
